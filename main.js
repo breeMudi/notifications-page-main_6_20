9d4c0e4096dd2acc1aacdb652784b522300f6002
breeMudi <65234249+breeMudi@users.noreply.github.com>
2023-04-17 22:32:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 const container = document.querySelector('.container')
+let totalUpdate = document.querySelectorAll('.update')
+// console.log(totalUpdate.length)
+
 container.addEventListener('click',(e) =>
   {
+    // Get element class(es)
     let elementClass = e.target.className;
+    // If element has class(es)
     if (elementClass !== '') {
         if(e.target.parentElement.className === 'inline'){
             if(e.target.parentElement.parentElement.parentElement.className === 'update'){
-                console.log(e.target.parentElement.parentElement.parentElement.className)
+                // console.log(e.target.parentElement.parentElement.parentElement.className)
             
                 e.target.parentElement.parentElement.parentElement.setAttribute('class', 'read-update')
-                console.log(e.target.parentElement.parentElement.parentElement.className)
+                // console.log(e.target.parentElement.parentElement.parentElement.className)
+
+                document.querySelector('.number-of-notifications').textContent = document.querySelectorAll('.update').length;
             }
         }
     }
     if(elementClass === 'read'){
         document.querySelectorAll('.update').forEach(e => e.classList.replace('update', 'read-update'))
+        document.querySelector('.number-of-notifications').textContent = document.querySelectorAll('.update').length;
     }
   }
 )
-
 
