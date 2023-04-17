f1f8b729c58b2b2873a5bb453b359bbb4e0891d3
breeMudi <65234249+breeMudi@users.noreply.github.com>
2023-04-17 21:14:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-const update = document.getElementsByClassName("update");
+const container = document.querySelector('.container')
+container.addEventListener('click',(e) =>
+  {
+    let elementClass = e.target.className;
+    if (elementClass !== '') {
+        if(e.target.parentElement.className === 'inline'){
+            if(e.target.parentElement.parentElement.parentElement.className === 'update'){
+                console.log(e.target.parentElement.parentElement.parentElement.className)
+            
+                e.target.parentElement.parentElement.parentElement.setAttribute('class', 'read-update')
+                console.log(e.target.parentElement.parentElement.parentElement.className)
+            }
+        }
+    }
+    if(elementClass === 'read'){
+        document.querySelectorAll('.update').forEach(e => e.classList.replace('update', 'read-update'))
+    }
+  }
+)
 
-const updatePressed = (e) => {
-  e.target.className = "read-update";
-}
 
-update[0].addEventListener("click", updatePressed);
